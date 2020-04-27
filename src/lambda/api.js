dcbb909084e1d6c5583a1904f864e34c6b38d1cd
Gauthier Gilles <g.gauthier@lexik.fr>
2020-04-27 11:51:28 +0200

    all

---
+++ src/lambda/api.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express')
+const cors = require('cors')
+
+const serverless = require('serverless-http');
+
+const app = express();
+
+app.use(cors());
+
+const router = express.Router();
+
+router.get('/hello', function (req, res) {
+    res.send({
+        message: 'hello'
+    })
+})
+
+app.use(process.env.API_BASE_PATH, router);
+
+module.exports.handler = serverless(app);
